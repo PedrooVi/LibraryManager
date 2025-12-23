8cf310a6f9799f23b1d725902c09dd38638a8015
PedrooVi <pedroviniciuscorrea@outlook.com>
2025-12-23 09:48:30 -0300

    Criando entidades

---
+++ src/main/java/com/projectLibrary/libraryManager/entity/Autor.java
@@ -0,0 +1,32 @@
+package com.projectLibrary.libraryManager.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// Entity ou entidade se refere as entidades do nosso projeto, ou seja tudo que sera uma tabela no banco de dados.
+// As entidades se encontram no package "Entity" do nosso projeto e precisam ser mapeadas usando anotacoes JPA "@Entity"
+@Entity
+// Vamos utilizar tambem a biblioteca Lombok que serve para eliminar a repeticao de codigo dentro do noso projeto Java, como por exemplo os metodos getters e setters
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Autor {
+    // Apos mapear nossa entidade precisamos tambem mapear nosso id para identificacao da nossa chave primaria
+    @Id
+    // Apos isso precisamos definir uma estrategia para geracao do nosso ID
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    //Precisamos tambem mapear nosso atributos como colunas e definir algumas restricoes
+    @Column(nullable = false, unique = true)
+    private String nome;
+
+    @Column(nullable = false)
+    private int idade;
+
+
+
+}
---
+++ src/main/java/com/projectLibrary/libraryManager/entity/Livro.java
@@ -0,0 +1,28 @@
+package com.projectLibrary.libraryManager.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// Entity ou entidade se refere as entidades do nosso projeto, ou seja tudo que sera uma tabela no banco de dados.
+// As entidades se encontram no package "Entity" do nosso projeto e precisam ser mapeadas usando anotacoes JPA "@Entity"
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Livro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private string nome;
+
+    // Agora precisamo mapear o relacionamento com a tabela autor no banco de dados, e para isso usamos o mapeamento da propria entidade usando a anotacao "@ManyToOne" para dizer que este e um relacionamento muitos para um, oque indica que um livro tem apenas um autor, mas um autor pode ter varios livros
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE} )
+    @JoinColumn(name = "autor_id")
+    private Autor autor;
+}
