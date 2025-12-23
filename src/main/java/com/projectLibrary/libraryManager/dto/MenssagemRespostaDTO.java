d797b4114ca7e557640b6f7cc3350da77e42d847
PedrooVi <pedroviniciuscorrea@outlook.com>
2025-12-23 11:22:25 -0300

    Criacao da classe Service para mapear as requisicoes da aplicacao

--- src/main/java/com/projectLibrary/libraryManager/dto/MenssagemRespostaDTO.java
+++ src/main/java/com/projectLibrary/libraryManager/dto/MenssagemRespostaDTO.java
@@ -1,4 +1,13 @@
 package com.projectLibrary.libraryManager.dto;
 
+import lombok.Builder;
+import lombok.Data;
+
+
+//Criamos ess classe para retornar as menssagens do nosso metodo
+// Vamos utilizar o Lombok para a implementacao da classe
+@Data
+@Builder
 public class MenssagemRespostaDTO {
+    private String menssagem;
 }
--- src/main/java/com/projectLibrary/libraryManager/service/LivroService.java
+++ src/main/java/com/projectLibrary/libraryManager/service/LivroService.java
@@ -1,4 +1,28 @@
 package com.projectLibrary.libraryManager.service;
 
+import com.projectLibrary.libraryManager.dto.MenssagemRespostaDTO;
+import com.projectLibrary.libraryManager.entity.Livro;
+import com.projectLibrary.libraryManager.repository.LivroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+// Vamos utilizar a anotacao "@Service" para indicar que essa classe e um service
+// A classe service e responsvel pela regra de negocio e logica de toda a minha aplicacao. Nela devem ser atribuidos os metodos de CRUD da nossa entidade, e esse metodos serao mapeados e utilizados para requisicao na classe controller
+@Service
 public class LivroService {
+    private LivroRepository livroRepository;
+
+    @Autowired
+    public LivroService(LivroRepository livroRepository) {
+        this.livroRepository = livroRepository;
+    }
+
+    // Vamos agora criar uma classe para retornar as nossas respostas das requisicoes.
+    // Criamos uma clase DTO para controlar a menssagem da nossa requisicao. Usamos o lombak com a anotacao "@Build" para criacao da classe
+    public MenssagemRespostaDTO create( Livro livro){
+        Livro livroSalvo = livroRepository.save(livro);
+        return MenssagemRespostaDTO.builder()
+                .menssagem("livro criado com o ID: " + livroSalvo.getId())
+                .build();
+    }
 }
