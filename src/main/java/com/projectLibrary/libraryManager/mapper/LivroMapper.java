cabc45c950367783a877adbce529e03d7281d709
PedrooVi <pedroviniciuscorrea@outlook.com>
2025-12-26 10:37:10 -0300

    Criando mapper para converter dados DTO em entidades e vice versa

--- src/main/java/com/projectLibrary/libraryManager/mapper/LivroMapper.java
+++ src/main/java/com/projectLibrary/libraryManager/mapper/LivroMapper.java
@@ -1,4 +1,17 @@
 package com.projectLibrary.libraryManager.mapper;
 
+import com.projectLibrary.libraryManager.dto.LivroDTO;
+import com.projectLibrary.libraryManager.entity.Livro;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+
+// Vamos utilizar um mapper para realizar a conversao do modelo de livro para um DTO e vice versa
+@Mapper
 public interface LivroMapper {
+
+    LivroMapper INSTANCE = Mappers.getMapper(LivroMapper.class);
+
+    Livro toModel(LivroDTO livroDTO);
+    LivroDTO toDTO(Livro livro);
 }
